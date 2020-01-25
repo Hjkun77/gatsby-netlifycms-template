2b43b99ac2120fff2e203851e8b068c004b55a3d
Hjkun77 <harveyjaysison@gmail.com>
2020-01-25 12:24:17 +0800

    BlogPostPagePreview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,7 @@ import React from "react"
 import CMS, { init } from "netlify-cms-app"
 import CSSInjector from "../components/CSSInjector";
 import HomePagePreview from "./preview-templates/HomePagePreview"
-
+import BlogPostPagePreview from "./preview-templates/BlogPostPagePreview"
 
 // Manually initialize CMS
 // if (process.env.GATSBY_IS_CMS === 'true') {
@@ -14,5 +14,12 @@ init()
 CMS.registerPreviewTemplate("index", props => (
   <CSSInjector>
     <HomePagePreview {...props} />
+  </CSSInjector>
+))
+
+
+CMS.registerPreviewTemplate("blog_post", props => (
+  <CSSInjector>
+    <BlogPostPagePreview {...props} />
   </CSSInjector>
 ))
