af7585e013318ea401eb9a6c4b32a6bebf92f1d3
Hjkun77 <harveyjaysison@gmail.com>
2020-01-25 12:12:55 +0800

    fix gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,12 +55,20 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-netlify-cms",
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
         publicPath: `admin`,
-        manualInit: true
-      }
+        manualInit: true,
+        enableIdentityWidget: false,
+        customizeWebpackConfig: (config, { plugins }) => {
+          config.plugins.push(
+            plugins.define({
+              __MANIFEST_PLUGIN_HAS_LOCALISATION__: JSON.stringify('false'),
+            }),
+          );
+        },
+      },
     },
     // `gatsby-plugin-feed-mdx`,
     // {
